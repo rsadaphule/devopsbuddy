97bb8643a69bd72479104c01979c3329f4d63397
Ravindra Sadaphule <rsadaphule@gmail.com>
2017-03-06 22:37:19 -0800

    added hello world rest controller

---
+++ src/main/java/com/devopsbuddy/controllers/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.devopsbuddy.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+	
+	@RequestMapping("/")
+	public String SayHelloWorld()
+	{
+		return "index";
+	}
+
+}
